9de1f0af4e3413863e6230cb2192bd6ac97dc9f3
tari2025 <denilestari272@gmail.com>
2025-06-02 10:49:49 +0700

    case base class

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+}
