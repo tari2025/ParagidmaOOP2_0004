79e48c3cf2adc55a7dc033e05d596184fcbc488f
tari2025 <denilestari272@gmail.com>
2025-06-02 11:18:16 +0700

    polimorpi

---
+++ polimorpi/polimorpi.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    // cout <<" pesan dari seseorang" << endl;}
+    // }
+};
+class joko : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia : public seseorang public : void pesan()
+{
+    cout << "Pesan dari lia" << endl;
+}
+}
+;
+int main()
+{
+    seseorang *obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    // a.sesorang::pesan();
+    // b.seseorang::pesan();
+
+    return;
+}
