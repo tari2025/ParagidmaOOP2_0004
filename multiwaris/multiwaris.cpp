b27aeee411272ade59a1f988f657fb2a119328d7
tari2025 <denilestari272@gmail.com>
2025-06-02 10:33:54 +0700

    class pelajar virtual public orang

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -23,4 +23,12 @@ public:
         cout << "pekerja dibuat\n"
              << endl;
     }
+} class pelajar : virtual public orang
+{
+public:
+    pelajar(int pUmur);
+    orang(pUmur)
+} cout << "pelajar dibuat\n"
+       << endl;
 }
+;
