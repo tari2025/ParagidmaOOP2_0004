a0a27f736a696f689d01a08dafede2e71e6c05de
tari2025 <denilestari272@gmail.com>
2025-06-02 09:55:43 +0700

    library

---
+++ abtraksi/abstraksi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas
+{
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+}
